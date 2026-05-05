cc7b471da132a0b7de2bf0e2895f9a44e730de35
twimpse <concept@twimp.se>
2026-05-05 17:52:21 +0200

    Commit

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+SET_YES=0
+
+while getopts "h:y:" arg; do
+  case $arg in
+    h)
+      echo "Usage"
+      ;;
+    y)
+      SET_YES=1
+      ;;
+  esac
+done
+
 sudo apt -qq update
 sudo apt -qq -y install python3 git mc screen 7zip unzip net-tools pwgen lsof sudo fail2ban iptables rkhunter harden-tools
 cd
 echo 'alias ls="ls --color"' >> ~/.bashrc
 source ~/.bashrc
-
-mkdir -p ~/.ssh
-chmod go-rwx ~/.ssh
-cat conf.d/authorized_keys >> ~/.ssh/authorized_keys
-chmod go-rwx ~./ssh/authorized_keys
 
 git clone https://github.com/twimpse/linux-conf.git
 cd linux-conf
